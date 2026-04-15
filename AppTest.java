ab79c9b93a17e6cb14cd0b7c94f44b1366beaff6
Kirankumar0706 <kirankumar2005p@gmail.com>
2026-04-15 15:23:35 +0530

    Refactor testAdd to improve readability
    Refactor testAdd method to store result in a variable and print it.

--- AppTest.java
+++ AppTest.java
@@ -5,6 +5,10 @@ public class AppTest {
     @Test
     public void testAdd() {
         App app = new App();
-        assertEquals(5, app.add(2, 3));
+        int result = app.add(2, 3);
+        
+        System.out.println("Result of addition: " + result);
+        
+        assertEquals(5, result);
     }
 }
